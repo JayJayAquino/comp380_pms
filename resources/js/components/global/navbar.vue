646e9aed49fe9c04b37e4c5626e7a29891bcc08c
jazperez <jackkiey24@gmail.com>
2019-05-07 13:13:58 -0700

    Add some more css, add background colors for images meanwhile the bug exists

--- resources/js/components/global/navbar.vue
+++ resources/js/components/global/navbar.vue
@@ -1,12 +1,15 @@
 <template>
 <div class="row">
-    <div class="navbar">
-        <div class="navbar__left">
-            <h class="navbar_title">PMS</h>
-        </div>
-        <div class="navbar__right">
-            <div class="navbar__left-img"></div>
-        </div>
+    <div class="col-md-12">
+        <div class="navbar">
+            <div class="navbar__left">
+                <div class="fas fa-bars"></div>
+                <h class="navbar_title">PMS</h>
+            </div>
+            <div class="navbar__right">
+                <div class="navbar__left-img"></div>
+            </div>
+    </div>
     </div>
 </div>
 </template>
--- resources/js/components/global/subHeader.vue
+++ resources/js/components/global/subHeader.vue
@@ -1,6 +1,6 @@
-<template>
+<!-- <template>
     <div class="subHeader">
         <h3>Project Management System</h3>
         <h6>Fruit Snaccs Inc.</h6>
 </div>
-</template>
+</template> -->
