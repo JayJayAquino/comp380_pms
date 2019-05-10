b45f998d8484552f3f3be77f9499ff9c8cfeb682
jazperez <jackkiey24@gmail.com>
2019-05-10 11:24:39 -0700

    Fix little bugs and finish Decisions, including with modal

--- resources/js/router/views/decisions/index.vue
+++ resources/js/router/views/decisions/index.vue
@@ -2,7 +2,7 @@
 <div class="col-md-10 body-container">
     <div class="row">
         <div class="col-md-12">
-            <p>Decisions</p>
+            <h4>Decisions</h4>
         </div>
         <div class="col-md-12">
             <table>
@@ -52,7 +52,16 @@
                 <div class="col-md-12 active-margin">
                 <div class="modal-content">
                     <div class="modal-header modal-dec">
-                        <h4 class="modal-title">Active (ID: AB-227)</h4>
+                        <h5 class="modal-title">Active (ID: AB-227)</h5>
+                        <div class="pull-right">CLOSED</div>
+                    </div>
+                    <div class="modal-header modal-sub-dec">
+                            <div>Date Raised: 02/28/18</div>
+                            <div>Date Assigned: 08/28/18</div>
+                            <div>Last Updated: 08/28/18</div>
+                            <div>Estimated End Date: 12/28/18</div>
+                            <div>Actual: 12/31/18</div>
+                            <div>Date Needed: 01/01/19</div>
                     </div>
             <div class="modal-body">
                 <form>
@@ -62,12 +71,36 @@
                             <input type="text" name="Title">
                         </div>
                         <div class="col-md-6">
-                            Expected End Date:<br>
-                            <input type="text" name="Expected End Date">
+                            Decision Maker:<br>
+                            <input type="text" name="Decision Maker">
+                        </div>
+                        <div class="col-md-6">
+                            Date Needed:<br>
+                            <input type="text" name="Date Needed">
+                        </div>
+                        <div class="col-md-6">
+                            Estimated Completion:<br>
+                            <input type="text" name="Estimated Completion">
+                        </div>
+                        <div class="col-md-6">
+                            Priority:<br>
+                            <input type="text" name="Priority">
+                        </div>
+                        <div class="col-md-6">
+                            Severity:<br>
+                            <input type="text" name="Severity">
                         </div>
                         <div class="col-md-6">
                             Description:<br>
                             <input type="text" name="Description">
+                        </div>
+                        <div class="col-md-6">
+                            Status:<br>
+                            <input type="text" name="Status" value="closed">
+                        </div>
+                        <div class="col-md-6">
+                            Status Description:<br>
+                            <input type="text" name="Status Description">
                         </div>
                     </div>
                 </form> 
@@ -79,37 +112,25 @@
         </div>
 
         <div class="col-md-12 active-margin">
-            <p>Related Requirements</p>
+            <p>Related Reference Documents</p>
         </div>
         <div class="col-md-12">
             <table>
                 <tr>
                     <th>ID</th>
                     <th>Title</th>
-                    <th>Due Date</th>
-                    <th>Assigned</th>
-                    <th>Description</th>
                 </tr>
                 <tr>
                     <td>3612</td>
-                    <td>Requirement</td>
-                    <td>02-18-2019</td>
-                    <td>Barb B.</td>
-                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Reference Documents</td>
                 </tr>
                 <tr>
                     <td>3612</td>
-                    <td>Requirement</td>
-                    <td>02-18-2019</td>
-                    <td>Joan J.</td>
-                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Reference Documents</td>
                 </tr>
                 <tr>
                     <td>3612</td>
-                    <td>Requirement</td>
-                    <td>02-18-2019</td>
-                    <td>Zylon Z.</td>
-                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Reference Documents</td>
                 </tr>
             </table>
         </div>
@@ -118,37 +139,25 @@
         </div>
 
         <div class="col-md-12 active-margin">
-            <p>Related Tasks</p>
+            <p>Meeting Notes</p>
         </div>
         <div class="col-md-12">
             <table>
                 <tr>
                     <th>ID</th>
                     <th>Title</th>
-                    <th>Due Date</th>
-                    <th>Assigned</th>
-                    <th>Description</th>
                 </tr>
                 <tr>
                     <td>3612</td>
-                    <td>Task</td>
-                    <td>02-18-2019</td>
-                    <td>Barb B.</td>
-                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Meeting Notes</td>
                 </tr>
                 <tr>
                     <td>3612</td>
-                    <td>Task</td>
-                    <td>02-18-2019</td>
-                    <td>Joan J.</td>
-                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Meeting Notes</td>
                 </tr>
                 <tr>
                     <td>3612</td>
-                    <td>Task</td>
-                    <td>02-18-2019</td>
-                    <td>Zylon Z.</td>
-                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Meeting Notes</td>
                 </tr>
             </table>
         </div>
@@ -156,6 +165,51 @@
             <button type="button" class="btn button-add table-button" data-toggle="modal" data-target="#myModal">Add</button>
         </div>
     </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+    
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header modal-dec">
+                <h4 class="modal-title">Decision Creation</h4>
+                <button type="button" class="close" data-dismiss="modal" style="color:white;">&times;</button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="row">
+                        <div class="col-md-6">
+                            Title:<br>
+                            <input type="text" name="Title">
+                        </div>
+                        <div class="col-md-6">
+                            Resource:<br>
+                            <input type="text" name="Resources">
+                        </div>
+                        <div class="col-md-6">
+                            Description:<br>
+                            <input type="text" name="Description">
+                        </div>
+                        <div class="col-md-6">
+                            Status:<br>
+                            <input type="text" name="Status">
+                        </div>
+                        <div class="col-md-6">
+                            Status Description:<br>
+                            <input type="text" name="Status Description">
+                        </div>
+                    </div>
+                </form> 
+            </div>
+            <div>
+                <button type="button" class="modal-footer modal-foot-dec" data-dismiss="modal"><div style="margin-right:45%;">Create</div></button>
+            </div>
+        </div>
+        </div>
+    </div>
+
 </div>
 </template>
 
